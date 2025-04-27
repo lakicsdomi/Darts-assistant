f6e0e1041b53674810d596d45f8ee65f04241a9d
Domonkos Lakics <lakicsdomi@gmail.com>
2025-04-28 00:49:04 +0200

    Add Avalonia-based DartsAssistant project, update .gitignore

---
+++ src/DartsAssistant/DartsAssistant/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DartsAssistant.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
