ed9504fc6cedb3454555ba11a956b574b0019b7f
Domonkos Lakics <lakicsdomi@gmail.com>
2025-05-09 16:07:33 +0200

    Add a few new classes

--- src/DartsAssistant/DartsAssistant/ViewModels/MainWindowViewModel.cs
+++ src/DartsAssistant/DartsAssistant/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,18 @@
-﻿namespace DartsAssistant.ViewModels
+﻿using System.Collections.ObjectModel;
+
+namespace DartsAssistant.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
         public string Greeting { get; } = "Welcome to Avalonia!";
+        public ObservableCollection<string> NumericButtons { get; } = new();
+
+        public MainWindowViewModel()
+        {
+            for (int i = 1; i < 21; i++)
+            {
+                NumericButtons.Add(i.ToString());
+            }
+        }
     }
 }
