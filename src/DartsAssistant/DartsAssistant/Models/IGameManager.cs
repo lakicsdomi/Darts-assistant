b50ee52813121176d65957c56404541cc1ee8cf7
Domonkos Lakics <lakicsdomi@gmail.com>
2025-05-09 16:00:58 +0200

    Add `IGameManager.cs`

---
+++ src/DartsAssistant/DartsAssistant/Models/IGameManager.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DartsAssistant.Models
+{
+    public interface IGameManager
+    {
+        public List<Player> players { get; set; }
+        public GameMode GameMode { get; set; }
+        public Player CurrentPlayer { get; set; }
+        public List<int> CurrentThrows { get; set; }
+        public void StartGame(GameMode gameMode, ESectorType checkout);
+        public void NextThrow(int points);
+        public void NextPlayer();
+        public void EndGame();
+    }
+}
